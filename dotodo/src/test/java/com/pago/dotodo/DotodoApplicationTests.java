5a8318982982b1f45f28aa7c5becd1d7cb6c202d
Stefan Hristov <sihristov@hotmail.com>
2024-04-18 22:25:51 +0300

    Initialize new project

---
+++ dotodo/src/test/java/com/pago/dotodo/DotodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pago.dotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
