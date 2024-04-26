9269fd856f57a0e04a543d30f9890c6d5ebe5c75
Stefan Hristov <sihristov@hotmail.com>
2024-04-27 00:09:41 +0300

    refactor configurations

---
+++ dotodo/src/main/java/com/pago/dotodo/configuration/AppBeanConfiguration.java
@@ -0,0 +1,16 @@
+package com.pago.dotodo.configuration;
+
+import com.pago.dotodo.model.dto.binding.UserTokenDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Configuration
+public class AppBeanConfiguration {
+
+    @Bean
+    @SessionScope
+    public UserTokenDto loggedUser() {
+        return new UserTokenDto();
+    }
+}
