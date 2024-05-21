366755b1d9e545d3d62009cae3c06cb1640cd3e0
Stefan Hristov <sihristov@hotmail.com>
2024-05-21 22:44:17 +0300

    fix bug with lack of attributes in the view

--- dotodo/src/main/java/com/pago/dotodo/web/BaseController.java
+++ dotodo/src/main/java/com/pago/dotodo/web/BaseController.java
@@ -5,13 +5,19 @@ import org.springframework.web.servlet.ModelAndView;
 public abstract class BaseController {
     private static final String REDIRECT = "redirect:";
 
-    public ModelAndView view(String viewName, ModelAndView modelAndView) {
-        modelAndView.setViewName(viewName);
-        return modelAndView;
-    }
-    public ModelAndView view (String viewName) {
+    protected ModelAndView view(String viewName) {
         return this.view(viewName, new ModelAndView());
     }
+
+    protected ModelAndView view(String viewName, Object... attributes) {
+        ModelAndView modelAndView = new ModelAndView(viewName);
+
+        for (int i = 0; i < attributes.length; i += 2) {
+            modelAndView.addObject((String) attributes[i], attributes[i + 1]);
+        }
+        return modelAndView;
+    }
+
     public ModelAndView redirect(String url) {
         return this.view(REDIRECT + url);
     }
