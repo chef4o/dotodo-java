8e651f2e7353f445e489f58d164904b67304cc86
Stefan Hristov <sihristov@hotmail.com>
2024-05-21 23:45:20 +0300

    add auth form service

--- dotodo/src/main/java/com/pago/dotodo/service/AuthService.java
+++ dotodo/src/main/java/com/pago/dotodo/service/AuthService.java
@@ -1,12 +1,10 @@
 package com.pago.dotodo.service;
 
 import com.pago.dotodo.model.dto.UserDto;
-import com.pago.dotodo.model.dto.UserLoginDto;
 import com.pago.dotodo.model.dto.UserRegisterDto;
 import com.pago.dotodo.model.dto.binding.UserTokenDto;
 import com.pago.dotodo.model.entity.User;
 import com.pago.dotodo.repository.UserRepository;
-import org.apache.commons.validator.routines.EmailValidator;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
--- dotodo/src/main/java/com/pago/dotodo/web/AuthController.java
+++ dotodo/src/main/java/com/pago/dotodo/web/AuthController.java
@@ -1,17 +1,48 @@
 package com.pago.dotodo.web;
 
 import com.pago.dotodo.model.dto.binding.UserTokenDto;
+import com.pago.dotodo.service.SkeletonService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController extends BaseController {
 
+    private static final String LOGIN_PAGE_NAME = "login";
+    private static final String REGISTER_PAGE_NAME = "register";
     private final UserTokenDto loggedUser;
 
-    public AuthController(UserTokenDto loggedUser) {
+    private final SkeletonService skeletonService;
+
+    public AuthController(UserTokenDto loggedUser, SkeletonService skeletonService) {
         this.loggedUser = loggedUser;
+        this.skeletonService = skeletonService;
     }
 
+    @GetMapping("/login")
+    public ModelAndView getLogin() {
+        return this.view("index",
+                "pageName", LOGIN_PAGE_NAME,
+                "tasks", skeletonService.getHomeItems(),
+                "topbarNavItems", skeletonService.getTopbarNavItems(),
+                "sidebarNavItems", skeletonService.getSidebarNavItems(),
+                "bottombarNavItems", skeletonService.getBottombarNavItems(),
+                "connectNavItems", skeletonService.getConnectNavItems()
+        );
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegister() {
+        return this.view("index",
+                "pageName", REGISTER_PAGE_NAME,
+                "tasks", skeletonService.getHomeItems(),
+                "topbarNavItems", skeletonService.getTopbarNavItems(),
+                "sidebarNavItems", skeletonService.getSidebarNavItems(),
+                "bottombarNavItems", skeletonService.getBottombarNavItems(),
+                "connectNavItems", skeletonService.getConnectNavItems()
+        );
+    }
 }
