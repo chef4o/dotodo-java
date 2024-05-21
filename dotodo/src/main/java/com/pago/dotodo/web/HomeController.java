3955e680c673c39c6693dc3a52ac0ffd6e3a3b68
Stefan Hristov <sihristov@hotmail.com>
2024-05-21 22:36:07 +0300

    add attributes to the home controller

--- dotodo/src/main/java/com/pago/dotodo/web/HomeController.java
+++ dotodo/src/main/java/com/pago/dotodo/web/HomeController.java
@@ -1,18 +1,30 @@
 package com.pago.dotodo.web;
 
-import org.springframework.web.bind.annotation.*;
+import com.pago.dotodo.service.SkeletonService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/")
 public class HomeController extends BaseController {
+    private static final String PAGE_NAME = "home";
+    private final SkeletonService skeletonService;
 
-    public HomeController() {
+    public HomeController(SkeletonService skeletonService) {
+        this.skeletonService = skeletonService;
     }
 
     @GetMapping
-    public ModelAndView getHome(ModelAndView modelAndView) {
-        modelAndView.addObject("pageName", "home");
-        return super.view("index");
+    public ModelAndView getHome() {
+        return this.view("index",
+                "pageName", PAGE_NAME,
+                "tasks", skeletonService.getHomeItems(),
+                "topbarNavItems", skeletonService.getTopbarNavItems(),
+                "sidebarNavItems", skeletonService.getSidebarNavItems(),
+                "bottombarNavItems", skeletonService.getBottombarNavItems(),
+                "connectNavItems", skeletonService.getConnectNavItems()
+        );
     }
 }
