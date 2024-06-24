a1907933bca107fa484474f316343387b91a2ef8
Stefan Hristov <sihristov@hotmail.com>
2024-06-25 02:16:43 +0300

    Add RoleEntity mapping

--- dotodo/src/main/java/com/pago/dotodo/configuration/AppBeanConfiguration.java
+++ dotodo/src/main/java/com/pago/dotodo/configuration/AppBeanConfiguration.java
@@ -2,19 +2,31 @@ package com.pago.dotodo.configuration;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.pago.dotodo.model.entity.RoleEntity;
 import com.pago.dotodo.model.enums.RoleEnum;
+import com.pago.dotodo.repository.RoleRepository;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.spi.MappingContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Configuration
 public class AppBeanConfiguration {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public AppBeanConfiguration(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
     @Bean
     public Gson gson() {
         return new GsonBuilder()
@@ -48,6 +60,9 @@ public class AppBeanConfiguration {
         modelMapper.addConverter(new Converter<String, RoleEnum>() {
             @Override
             public RoleEnum convert(MappingContext<String, RoleEnum> mappingContext) {
+                if (mappingContext.getSource() == null || mappingContext.getSource().isEmpty()) {
+                    return null;
+                }
                 return RoleEnum.valueOf(mappingContext.getSource().toUpperCase());
             }
         });
@@ -55,15 +70,35 @@ public class AppBeanConfiguration {
         modelMapper.addConverter(new Converter<RoleEnum, String>() {
             @Override
             public String convert(MappingContext<RoleEnum, String> mappingContext) {
-                return mappingContext.getSource()
-                        .toString()
-                        .substring(0, 1)
-                        .toUpperCase()
-                        + mappingContext.getSource()
-                        .toString()
-                        .substring(1)
-                        .toLowerCase()
-                        .replaceAll("_", "\s");
+                if (mappingContext.getSource() == null) {
+                    return null;
+                }
+                return mappingContext.getSource().toString().substring(0, 1).toUpperCase()
+                        + mappingContext.getSource().toString()
+                        .substring(1).toLowerCase()
+                        .replaceAll("_", " ");
+            }
+        });
+
+        modelMapper.addConverter(new Converter<String, RoleEntity>() {
+            @Override
+            public RoleEntity convert(MappingContext<String, RoleEntity> mappingContext) {
+                if (mappingContext.getSource() == null || mappingContext.getSource().isEmpty()) {
+                    return null;
+                }
+                RoleEnum roleEnum = RoleEnum.valueOf(mappingContext.getSource().toUpperCase());
+                Optional<RoleEntity> roleEntityOptional = roleRepository.findRoleEntityByRole(roleEnum);
+                return roleEntityOptional.orElse(null);
+            }
+        });
+
+        modelMapper.addConverter(new Converter<RoleEntity, String>() {
+            @Override
+            public String convert(MappingContext<RoleEntity, String> mappingContext) {
+                if (mappingContext.getSource() == null) {
+                    return null;
+                }
+                return mappingContext.getSource().getRole().toString();
             }
         });
 
