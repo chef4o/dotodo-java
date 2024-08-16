b291df4dc57a8d3bb0b556f299d4863b4d6c18fc
Stefan Hristov <sihristov@hotmail.com>
2024-08-16 15:59:17 +0300

    add calendar to events

--- dotodo/src/main/java/com/pago/dotodo/web/mvc/EventController.java
+++ dotodo/src/main/java/com/pago/dotodo/web/mvc/EventController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
 @Controller
 @RequestMapping("/events")
 public class EventController extends BaseController {
@@ -17,19 +19,18 @@ public class EventController extends BaseController {
     private static final String PAGE_NAME = "events";
     private final EventService eventService;
     private final ModelAndViewParser attributeBuilder;
-    private final DateTimeUtil dateTimeUtil;
 
     public EventController(EventService eventService, ModelAndViewParser attributeBuilder, DateTimeUtil dateTimeUtil) {
         this.eventService = eventService;
         this.attributeBuilder = attributeBuilder;
-        this.dateTimeUtil = dateTimeUtil;
     }
 
     @GetMapping
     public ModelAndView getEvents(@AuthenticationPrincipal CustomAuthUserDetails userDetails) {
         return this.view("index", attributeBuilder.build(
                 "pageName", PAGE_NAME,
-                "events", eventService.getAll(userDetails.getId()))
+                "events", eventService.getAll(userDetails.getId()),
+                "currentDate", new Date())
         );
     }
 }
